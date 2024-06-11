61ddf38791848d7984a47e2a8575a51adb7d8ad6
bsegers <briansegs@gmail.com>
2024-06-11 19:22:40 -0400

    add: hero section

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Hero from "./sections/Hero";
+
+function App() {
+  return (
+    <main className="App">
+      <section className="hero_container">
+        <Hero />
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
+import particlesOptions from "../particles_1.json";
+import logo from "../logo.svg";
+
+const Hero = () => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    if (init) {
+      return;
+    }
+
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  return (
+    <section className="hero_section">
+      <div className="hero_left">
+        {init && <Particles options={particlesOptions} />}
+        <div>
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+      </div>
+
+      <div className="hero_right"></div>
+    </section>
+  );
+};
+
+export default Hero;
